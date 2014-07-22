fd4015136078b68327c2ebe678d858a822d6e0a3
Nick Mitchinson <nrmitchi@gmail.com>
2014-07-21 21:41:06 -0400

    Fixing install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo 'You might need to change your default shell to zsh: `chsh -s /bin/zsh` (or `sudo vim /etc/passwd`)'
 
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-dir="$HOME/Developer/nrmitchi/dotfiles"
+dir="$HOME/Developer/nrmitchi"
 mkdir -p $dir
 cd $dir
 git clone git://github.com/nrmitchi/dotfiles.git
