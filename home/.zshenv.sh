0ba21cd65a4b73a2c2ea88ee39bef768e7ece363
Nick Mitchinson <nrmitchi@gmail.com>
2016-08-16 21:19:14 -0500

    Remove boot2docker stuff

--- home/.zshenv.sh
+++ home/.zshenv.sh
@@ -135,7 +135,3 @@ fi
 
 export NODE_ENV=development
 
-# boot2docker
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH=/Users/nrmitchi/.boot2docker/certs/boot2docker-vm
-export DOCKER_TLS_VERIFY=1
