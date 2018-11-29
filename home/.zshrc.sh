cdf399a38333cd051c1c0ad27908c5090a9c09a0
Nick Mitchinson <nrmitchi@gmail.com>
2018-11-29 11:18:56 -0600

    Switch to a more stable lazy-load for RVM

--- home/.zshrc.sh
+++ home/.zshrc.sh
@@ -304,16 +304,45 @@ function kubectl() {
     command kubectl "$@"
 }
 
+# ------ Stole fromn https://gist.github.com/QinMing/364774610afc0e06cc223b467abe83c0 ----
+lazy_load() {
+    # Act as a stub to another shell function/command. When first run, it will load the actual function/command then execute it.
+    # E.g. This made my zsh load 0.8 seconds faster by loading `nvm` when "nvm", "npm" or "node" is used for the first time
+    # $1: space separated list of alias to release after the first load
+    # $2: file to source
+    # $3: name of the command to run after it's loaded
+    # $4+: argv to be passed to $3
+    echo "Lazy loading $1 ..." > /dev/stderr
+
+    # $1.split(' ') using the s flag. In bash, this can be simply ($1) #http://unix.stackexchange.com/questions/28854/list-elements-with-spaces-in-zsh
+    # Single line won't work: local names=("${(@s: :)${1}}"). Due to http://stackoverflow.com/questions/14917501/local-arrays-in-zsh   (zsh 5.0.8 (x86_64-apple-darwin15.0))
+    local -a names
+    if [[ -n "$ZSH_VERSION" ]]; then
+        names=("${(@s: :)${1}}")
+    else
+        names=($1)
+    fi
+    unalias "${names[@]}"
+    . $2
+    shift 2
+    $*
+}
+
+group_lazy_load() {
+    local script
+    script=$1
+    shift 1
+    for cmd in "$@"; do
+        alias $cmd="lazy_load \"$*\" $script $cmd"
+    done
+}
+# ----------------------------------------------------------------------------------------
+
+
 # RVM thing
 # [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-
-function rvm() {
-    if ! type __rvm_has_opt >/dev/null 2>&1; then
-        [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-    fi
-
-    command rvm "$@"
-}
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+group_lazy_load $HOME/.rvm/scripts/rvm rvm irb rake rails
 
 # NVM thing
 # export NVM_DIR="$HOME/.nvm"
